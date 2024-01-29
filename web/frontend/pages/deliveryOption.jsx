a091e9d7914d515a545921525335c29f71ef51ff
Nami5011 <maron.011821@gmail.com>
2024-01-29 15:19:32 +0900

    loading on delivery option page

--- web/frontend/pages/deliveryOption.jsx
+++ web/frontend/pages/deliveryOption.jsx
@@ -3,6 +3,7 @@ import {
 	Text,
 	AlphaCard,
 	Link,
+	SkeletonBodyText
 } from "@shopify/polaris";
 import { useTranslation, Trans } from "react-i18next";
 import { useAppQuery, useAuthenticatedFetch } from "../hooks";
@@ -10,7 +11,6 @@ import { useState, useEffect, useCallback } from 'react';
 import { useAppBridge, Loading } from "@shopify/app-bridge-react";
 import { Redirect } from '@shopify/app-bridge/actions';
 import { checkAppSubscription } from "../utils/appSubscription";
-
 export default function HomePage() {
 	const { t } = useTranslation();
 	const fetch = useAuthenticatedFetch();
@@ -24,6 +24,7 @@ export default function HomePage() {
 		}
 		return true;
 	}
+	const [isLoading, setIsLoading] = useState(true);
 
 	const [deliveryOptionUrl, setDeliveryOptionUrl] = useState('/');
 	const init = async () => {
@@ -52,6 +53,7 @@ export default function HomePage() {
 		} catch (error) {
 			console.error('Error fetching data:', error);
 		}
+		setIsLoading(false);
 	};
 	useEffect(() => {
 		init();
@@ -68,14 +70,16 @@ export default function HomePage() {
 					{t("deliveryOption.instructionTitle")}
 				</Text>
 				<AlphaCard roundedAbove="sm">
-					<ol>
-						<li>{t("deliveryOption.instructions.1-1")}<Link url="/cartMessage">{t("deliveryOption.instructions.1-2")}</Link>{t("deliveryOption.instructions.1-3")}.</li>
-						<li>{t("deliveryOption.instructions.2-1")}<strong>{t("deliveryOption.instructions.2-2")}</strong>{t("deliveryOption.instructions.2-3")}<Link url={deliveryOptionUrl} external>{t("deliveryOption.instructions.2-4")}</Link>{t("deliveryOption.instructions.2-5")}.</li>
-						<li>{t("deliveryOption.instructions.3-1")}<strong>{t("deliveryOption.instructions.3-2")}</strong>{t("deliveryOption.instructions.3-3")}.</li>
-						<li>{t("deliveryOption.instructions.4-1")}<strong>{t("deliveryOption.instructions.4-2")}</strong>{t("deliveryOption.instructions.4-3")}.</li>
-						<li>{t("deliveryOption.instructions.5")}.</li>
-						<li>{t("deliveryOption.instructions.6")}.</li>
-					</ol>
+					{isLoading ? <SkeletonBodyText lines={5} /> :
+						<ol>
+							<li>{t("deliveryOption.instructions.1-1")}<Link url="/cartMessage">{t("deliveryOption.instructions.1-2")}</Link>{t("deliveryOption.instructions.1-3")}.</li>
+							<li>{t("deliveryOption.instructions.2-1")}<strong>{t("deliveryOption.instructions.2-2")}</strong>{t("deliveryOption.instructions.2-3")}<Link url={deliveryOptionUrl} external>{t("deliveryOption.instructions.2-4")}</Link>{t("deliveryOption.instructions.2-5")}.</li>
+							<li>{t("deliveryOption.instructions.3-1")}<strong>{t("deliveryOption.instructions.3-2")}</strong>{t("deliveryOption.instructions.3-3")}.</li>
+							<li>{t("deliveryOption.instructions.4-1")}<strong>{t("deliveryOption.instructions.4-2")}</strong>{t("deliveryOption.instructions.4-3")}.</li>
+							<li>{t("deliveryOption.instructions.5")}.</li>
+							<li>{t("deliveryOption.instructions.6")}.</li>
+						</ol>
+					}
 				</AlphaCard>
 			</div>
 			<div style={{ "marginBottom": '20px' }}>
